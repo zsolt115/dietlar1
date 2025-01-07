763bd69b855f3ada7185dea955f61a8047b6f343
zsolt115 <sikozsolt96@gmail.com>
2025-01-07 21:42:37 +0200

    new

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar></Navbar>
+      {children}
+    </main>
+  );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30}></Image>
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+      Navbar
+    </header>
+  );
+};
+
+export default Navbar;
