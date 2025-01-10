f0a353ca9e7d02ef963506277e7bb2999f8103f7
zsolt115 <sikozsolt96@gmail.com>
2025-01-10 15:27:30 +0200

    hero section, search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({searchParams}: {
+  searchParams: Promise<{query?: string}>
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className="text-2xl">HOME</h1>
+    <section className="pink_container">
+      <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+
+      <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+
+      <SearchForm query={query}/>
+    </section>
     </>
   );
 }
