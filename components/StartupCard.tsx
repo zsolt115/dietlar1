09f6fbd0f75fa9d16dcb0ac7a78be4a65f9761a2
zsolt115 <sikozsolt96@gmail.com>
2025-03-06 15:43:05 +0200

    first part done and first item shown on page | sanity is up

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,6 +3,8 @@ import {EyeIcon} from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import { Button } from "@/components/ui/button";
+
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const { _createdAt, views, author: { _id: authorId, name}, title, category, _id, image, description} = post;
@@ -14,38 +16,47 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                 {formatDate(_createdAt)}
             </p>
 
-            <div>
+            <div className='flex gap-1.5'>
               <EyeIcon className='size-6 text-primary' />
               <span className='text-16-medium'>{views}</span>
             </div>
-
-            <div className='flex-between met-5 gap-5'>
-              <Link href={`/user/${authorId}`}>
-                <p className='text-16-medium line-clamp-1'>
-                  {name}
-                </p>
-              </Link>
-              <Link href={`/startup/${_id}`}>
-                <h3 className='text-26-semibold line-clam-1'>{title}</h3>
-              </Link>
-              <Link href={`/user/$authorId}`}>
-                <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full"/>
-              </Link>
-
-              <Link href={`/startup/${_id}`}>
-                <p className='startup-card_desc'>
-                  {description}
-                </p>
-                <img src={image} alt="placeholder" className='startup-card_img' />
-              </Link>
-
-              <div className='flex-between gap-3 mt-5'>
-                <Link href={`/?query${category.toLowerCase()}`}>
-
-                </Link>
-              </div>
-            </div>
         </div>
+
+        <div className='flex-between mt-5 gap-5'>
+          <div className='flex-1'>
+            <Link href={`/user/${authorId}`}>
+              <p className='text-16-medium line-clamp-1'>
+                {name}
+              </p>
+            </Link>
+            <Link href={`/startup/${_id}`}>
+              <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
+            </Link>
+          </div>
+
+          <Link href={`/user/$authorId}`}>
+            <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full"/>
+          </Link>
+
+        </div>
+
+          <Link href={`/startup/${_id}`}>
+            <p className='startup-card_desc'>
+              {description}
+            </p>
+            <img src={image} alt="placeholder" className='startup-card_img' />
+          </Link>
+
+          <div className='flex-between gap-3 mt-5'>
+            <Link href={`/?query${category.toLowerCase()}`}>
+              <p className='text-16-medium'>{category}</p>
+            </Link>
+            <Button>
+              <Link href={`/startup{${_id}}`}>
+                Details
+              </Link>
+            </Button>
+          </div>
     </li>
   )
 }
