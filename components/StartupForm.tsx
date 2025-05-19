4951958024eb59cec293c2bde0d2cc05dacb89a7
zsolt115 <sikozsolt96@gmail.com>
2025-05-19 15:22:25 +0300

    implementing create new startup

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -10,6 +10,7 @@ import { formSchema } from '@/lib/validation';
 import { z } from "zod";
 import { useToast } from '@/hooks/use-toast';
 import { useRouter } from 'next/navigation';
+import { createPitch } from '@/lib/action';
 
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string, string>>({}); // 3:55:00
@@ -28,21 +29,19 @@ const StartupForm = () => {
             }
 
             await formSchema.parseAsync(formValues);
-            console.log(formValues);
 
-            // const result = await createIdea(prevState, formData, pitch);
+            const result = await createPitch(prevState, formData, pitch);
 
-            // console.log(result); 4:11:00
-            // if (result.status == 'SUCCESS') {
-            //     toast( {
-            //         title: "Success",
-            //         description: "Your startup pitch has been created successfully",
-            //     });
+            if (result.status == 'SUCCESS') {
+                toast( {
+                    title: "Success",
+                    description: "Your startup pitch has been created successfully",
+                });
 
-            //     router.push(`/startup/${result.id}`);
-            // }
+                router.push(`/startup/${result._id}`);
+            }
 
-            // return result;
+            return result;
         } catch (error) {
             if (error instanceof z.ZodError) {
                 const fieldErrors = error.flatten().fieldErrors;
--- lib/utils.ts
+++ lib/utils.ts
@@ -13,3 +13,7 @@ export function formatDate(date: string) {
     year: 'numeric',
   })
 }
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
+}
